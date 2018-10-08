4634ff34e427bee5958c5feee4f35706ba8fa767
OZAWA Sakuro <sakuro@2238.club>
2018-10-08 18:58:13 +0900

    Implement with_public method

--- lib/with_public.rb
+++ lib/with_public.rb
@@ -1,5 +1,26 @@
 require "with_public/version"
 
 module WithPublic
-  # Your code goes here...
+  # Returns a refiner module to make target methods public
+  #
+  # @param [Array<Symbol>] methods Method symbols to be made public
+  # @return [Module] a refinement module to make given methods public
+  # @example Make Foo#bar public
+  #     RSpec.describe Foo do
+  #       using Foo.with_public(:bar)
+  #     end
+  #
+  #     Calling Foo#bar will not raise NoMethodError in this context.
+  def with_public(*method_names)
+    target = self
+    Module.new.tap do |refiner|
+      refiner.module_eval do
+        refine target do
+          public(*method_names)
+        end
+      end
+    end
+  end
 end
+
+Module.include(WithPublic)
--- spec/with_public_spec.rb
+++ spec/with_public_spec.rb
@@ -1,9 +1,26 @@
+require 'with_public'
+
 RSpec.describe WithPublic do
-  it "has a version number" do
-    expect(WithPublic::VERSION).not_to be nil
+  class Target
+    private
+    def this_is_a_private_method
+      'called'
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:target) { Target.new }
+
+  context 'with using #with_public' do
+    using Target.with_public(:this_is_a_private_method)
+
+    it 'can call private methods' do
+      expect(target.this_is_a_private_method).to eq('called')
+    end
+  end
+
+  context 'without using #with_public' do
+    it 'cannot call private methods' do
+      expect { target.this_is_a_private_method }.to raise_error(NoMethodError)
+    end
   end
 end
